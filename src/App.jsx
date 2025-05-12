375e3754f4056514008bd9977661fec03a0a3a8b
kadirdolek <kadirdolek@outlook.com>
2025-05-12 16:42:30 +0200

    ajout

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,43 @@ import './App.css'
 import Image from './image/Image'
 import Button from './button/Button'
 import Progress from './progress/Progress'
+import Son from './assets/son/punch-2-37333.mp3'
 
 function App() {
   const [vie, setVie] = useState(100);
   const [scale, setScale] = useState(1);
+  const [rotate,SetRotate] = useState(0)
 
   const handlePunch = () => {
-    setVie((prev) => Math.max(prev - 10));
+    const son = new Audio(Son)
+    son.currentTime= 0.3
+    son.play()
+    // setVie((prev) => Math.max(prev - 10));
+    setVie (vie-10);
     setScale(0.9);
+    SetRotate(rotate+25);
 
     setTimeout(() => {
+      SetRotate(0);
+    }, 100);
+    setTimeout(() => {
       setScale(1);
-    }, 50);
+    }, 100);
+
+    
   };
 
   const Reset = () => {
     setVie(100);
-    setScale(1);
   };
+  
 
 
   return (
     <>
   <Progress vie={vie}/>
-  <Image vie={vie} scale={scale}/>
-  <Button punch={handlePunch} reset={Reset} />
+  <Image vie={vie} scale={scale} rotate={rotate}/>
+  <Button punch={handlePunch} reset={Reset} vie={vie} />
     </>
   )
 }
--- src/image/Image.jsx
+++ src/image/Image.jsx
@@ -1,14 +1,19 @@
-export default function Image({ scale, vie }) {
-
-
-const bg =
-    <img src="https://api.molengeek.education/storage/uploads/users/410/lesson/4051/bag.png" alt="" style={{transform: `scale(${scale})`}}/>
-
-const bg1 =
-    <img src="https://api.molengeek.education/storage/uploads/users/410/lesson/4051/bag-burst.png" alt="" />
-
-
-  return (
-    vie > 0 ? bg : bg1
+import "./Image.css";
+export default function Image({ scale, vie, rotate }) {
+  const bg = (
+    <img
+      src="https://api.molengeek.education/storage/uploads/users/410/lesson/4051/bag.png"
+      alt=""
+      style={{ transform: `scale(${scale}) rotate(${rotate}deg)` }}
+    />
   );
+
+  const bg1 = (
+    <img
+      src="https://api.molengeek.education/storage/uploads/users/410/lesson/4051/bag-burst.png"
+      alt=""
+    />
+  );
+
+  return vie > 0 ? bg : bg1;
 }
